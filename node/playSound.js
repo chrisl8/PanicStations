1cd278ebee8045a50c2fd0d5fbae2d05cb39cdb1
Christen Lofland <christen@lofland.net>
2023-06-03 22:33:00 -0500

    Fix volume control.

--- node/playSound.js
+++ node/playSound.js
@@ -1,10 +1,12 @@
 import { exec } from 'child_process';
-import settings from './settings.js';
 
-const playSound = (sound) => {
-  exec(
-    `amixer -q -M sset Headphone ${settings.volume.setting}%;aplay sounds/${sound}.wav`,
-  );
+/**
+ * @param {String} sound
+ * @param {Object} settings
+ */
+const playSound = ({ sound, settings }) => {
+  const playSoundExecCommand = `amixer -q -M sset ${settings.volume.alsaMixerDeviceName} ${settings.volume.setting}%;aplay sounds/${sound}.wav`;
+  exec(playSoundExecCommand);
 };
 
 export default playSound;
