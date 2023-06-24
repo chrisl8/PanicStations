f6024e31c25ad4fe414934481fffafee08244cb5
Christen Lofland <christen@lofland.net>
2023-06-24 14:11:38 -0500

    Cease using global lcdPort setting and use per station setting.

--- client/display.js
+++ client/display.js
@@ -7,8 +7,8 @@ async function initialize(settings) {
   if (settings.useScreen) {
     screen.initialize({ settings });
   }
-  if (settings.useLCD) {
-    for (const [, value] of Object.entries(settings.stations)) {
+  for (const [key, value] of Object.entries(settings.stations)) {
+    if (value.hasOwnProperty('lcdPort')) {
       const port = new UsbDevice(value.lcdPort.string, value.lcdPort.location);
       // NOTE: The LCD screens are set by their USB location, so plugging them in differently will cause them to get lost. The LCD screens have no serial numbers, so it is the only reliable way.
       try {
@@ -17,7 +17,7 @@ async function initialize(settings) {
         value.lcdPort.lcd = new DisplayLCD(value.lcdPort.name);
       } catch (e) {
         console.error(
-          `useLCD is turned on in the settings.json5 file, but an LCD screen was not found on any visible USB device.\n${e}`,
+          `Station ${key} has an lcdPort listed in the settings.json5 file, but an LCD screen was not found on any visible USB device.\n${e}`,
         );
         process.exit(1);
       }
@@ -34,16 +34,17 @@ async function initialize(settings) {
  * @param {Object} gameState
  */
 function update({ gameState, state, data, settings }) {
-  if (settings.useLCD) {
-    for (const [key, value] of Object.entries(settings.stations)) {
-      if (!data || !data.hasOwnProperty('station') || data.station === key) {
-        value.lcdPort.lcd.update({
-          state,
-          data,
-          gameState,
-          stationData: value,
-        });
-      }
+  for (const [key, value] of Object.entries(settings.stations)) {
+    if (
+      value.hasOwnProperty('lcdPort') &&
+      (!data || !data.hasOwnProperty('station') || data.station === key)
+    ) {
+      value.lcdPort.lcd.update({
+        state,
+        data,
+        gameState,
+        stationData: value,
+      });
     }
   }
   if (settings.useScreen) {
